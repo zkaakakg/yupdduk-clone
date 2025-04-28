9dd28e09be384eaef9155b89af1b12e2fda9da37
zkaakakag <cscom222@naver.com>
2025-04-28 15:59:11 +0900

    feat: 메인 화면을 위한 컨트롤러 추가

---
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.project.yupdduk_clone.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    public String main(){
+        return "메인 화면";
+    }
+}
