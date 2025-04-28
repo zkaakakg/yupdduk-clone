940552ec6cc3b29564521509f1384db889348148
zkaakakag <cscom222@naver.com>
2025-04-28 09:32:14 +0900

    chore: BE 프로젝트 초기 설정

---
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/YupddukCloneApplication.java
@@ -0,0 +1,13 @@
+package com.project.yupdduk_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YupddukCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YupddukCloneApplication.class, args);
+	}
+
+}
