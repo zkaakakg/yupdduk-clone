5b5ed9539a2a658ac1dc5ddb8b686b7351e3dc32
zkaakakag <cscom222@naver.com>
2025-04-29 09:32:35 +0900

    feat: UserService에 현재 로그인한 회원 조회 및 탈퇴 기능 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/service/UserService.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/service/UserService.java
@@ -1,10 +1,15 @@
 package com.project.yupdduk_clone.service;
 
 import com.project.yupdduk_clone.dto.UserDto;
+import com.project.yupdduk_clone.entity.User;
 import com.project.yupdduk_clone.entity.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +17,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void signUp(UserDto userDto){
+    public void signUp(UserDto userDto) {
         String encodedPassword = passwordEncoder.encode(userDto.getPassword());
         userDto.setPassword(encodedPassword);
         this.userRepository.save(userDto.toEntity());
 
     }
 
+    public Optional<User> getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserEmail = authentication.getName();
+
+        Optional<User> user = userRepository.findByEmail(currentUserEmail);
+        return user;
+    }
+
+    public void deleteUser(Optional<User> optionalUser) {
+        User user = optionalUser.get();
+        userRepository.delete(user);
+    }
 }
