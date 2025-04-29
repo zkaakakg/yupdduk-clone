01e381a08263a1ec9e87316add15d62922dee9d5
gaeun.jang <cscom222@naver.com>
2025-04-29 22:55:27 +0900

    feat: SnsUser 엔티티에 provider와 providerId 필드 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/SnsUser.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/SnsUser.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-@Table(name = "sns_user")
+@Table(name = "sns_users")
 public class SnsUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,15 +23,23 @@ public class SnsUser {
 
     private String picture;
 
+    @Column(nullable = false)
+    private String provider; // google, naver, kakao
+
+    @Column(nullable = false)
+    private String providerId; // 제공자의 고유 ID
+
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role", nullable = false)
     private UserRole role;
 
     @Builder
-    public SnsUser(String name, String email, String picture, UserRole role) {
+    public SnsUser(String name, String email, String picture,String provider, String providerId, UserRole role) {
         this.name = name;
         this.email = email;
         this.picture = picture;
+        this.provider=provider;
+        this.providerId=providerId;
         this.role = role;
     }
 
