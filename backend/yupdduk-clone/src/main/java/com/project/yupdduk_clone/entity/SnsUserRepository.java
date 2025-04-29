ee1db8e696f0d0e58764f915fd238dd7adb4cabd
gaeun.jang <cscom222@naver.com>
2025-04-29 22:56:09 +0900

    feat: SnsUserRepository에 provider와 providerId를 기준으로 사용자 조회하는 메소드 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/SnsUserRepository.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/SnsUserRepository.java
@@ -11,4 +11,5 @@ public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {
     //SNS 로그인으로 반환되는 값중에서 email을 통해
     //이미 생성된 사용자인지, 처음가입하는 사용자인지 구분하기 위한 메소드
     Optional<SnsUser> findByEmail(String email);
+    Optional<SnsUser> findByProviderAndProviderId(String provider, String providerId);
 }
