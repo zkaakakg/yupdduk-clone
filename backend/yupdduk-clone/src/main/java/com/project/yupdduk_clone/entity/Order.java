4c42a4612093bf75e0d425e55d7ee40a1e75b315
zkaakakag <cscom222@naver.com>
2025-04-30 17:34:28 +0900

    feat: 주문(Order) 엔티티 생성 및 연관관계 설정

---
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/Order.java
@@ -0,0 +1,54 @@
+package com.project.yupdduk_clone.entity;
+
+import com.project.yupdduk_clone.enumeration.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_time")
+    private LocalDateTime orderTime;
+
+    @Column(name = "total_price")
+    private Integer totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status")
+    private OrderStatus orderStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") // DB 컬럼명
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems;
+
+    @Builder
+    Order(Long id, LocalDateTime orderTime, int totalPrice, OrderStatus orderStatus, Store store, User user, List<OrderItem> orderItems) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.totalPrice = totalPrice;
+        this.orderStatus = orderStatus;
+        this.store = store;
+        this.user = user;
+        this.orderItems=orderItems;
+    }
+
+
+}
