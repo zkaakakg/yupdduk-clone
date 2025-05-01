d1f1a8623787da0387be067988facdb166c616bc
지원 <145041049+nowwizz@users.noreply.github.com>
2025-05-02 00:06:51 +0900

    design: 주문 페이지 2 데이터 추가 + 메인메뉴 퍼블리싱 완료

--- frontend/yupdduk-clone/src/pages/OrderPage1.jsx
+++ frontend/yupdduk-clone/src/pages/OrderPage1.jsx
@@ -26,6 +26,36 @@ const OrderPage1 = () => {
       name: "김포고촌점",
       address: "김포시 고촌읍 장차로5번길 10 1층103호",
     },
+    {
+      id: 1,
+      name: "김포사우점",
+      address: "경기도 김포시 돌문로86번길 12-18 (사우동) 1층 103호",
+    },
+    {
+      id: 2,
+      name: "김포풍무점",
+      address: "경기도 김포시 풍무로 51 (풍무동) 104호",
+    },
+    {
+      id: 3,
+      name: "김포운양점",
+      address: "경기도 김포시 한강1로 247(운양동), 김포운양헤리움타운 124호",
+    },
+    {
+      id: 4,
+      name: "김포고촌점",
+      address: "김포시 고촌읍 장차로5번길 10 1층103호",
+    },
+    {
+      id: 1,
+      name: "김포사우점",
+      address: "경기도 김포시 돌문로86번길 12-18 (사우동) 1층 103호",
+    },
+    {
+      id: 2,
+      name: "김포풍무점",
+      address: "경기도 김포시 풍무로 51 (풍무동) 104호",
+    },
   ];
 
   const filteredStores = stores.filter((store) =>
@@ -34,27 +64,29 @@ const OrderPage1 = () => {
 
   return (
     <div>
-      <Header title="방문 매장 찾기" />
-      <nav>
-        <div className="search">
-          <img
-            src="https://www.yupdduk.com/bj-images/yupdduk_sub/searchicon.png"
-            alt=""
-          />
-          <input
-            type="text"
-            placeholder="매장명 검색"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button onClick={() => setSearchTerm("")}>
+      <div className="fixed">
+        <Header title="방문 매장 찾기" />
+        <nav>
+          <div className="search">
             <img
-              src="	https://www.yupdduk.com/bj-images/yupdduk_sub/searchicon_close.png"
+              src="https://www.yupdduk.com/bj-images/yupdduk_sub/searchicon.png"
               alt=""
             />
-          </button>
-        </div>
-      </nav>
+            <input
+              type="text"
+              placeholder="매장명 검색"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={() => setSearchTerm("")}>
+              <img
+                src="	https://www.yupdduk.com/bj-images/yupdduk_sub/searchicon_close.png"
+                alt=""
+              />
+            </button>
+          </div>
+        </nav>
+      </div>
       <main>
         <ul className="storeList">
           {filteredStores.length > 0 ? (
@@ -73,6 +105,9 @@ const OrderPage1 = () => {
             </div>
           )}
         </ul>
+        <div
+          style={{ width: "100%", background: "white", padding: "4%" }}
+        ></div>
       </main>
     </div>
   );
