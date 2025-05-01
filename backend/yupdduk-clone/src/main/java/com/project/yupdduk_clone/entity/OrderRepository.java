e6cf4bfcef861ca2ed08252d6151a9d5abcc35f8
zkaakakag <cscom222@naver.com>
2025-05-01 17:29:35 +0900

    feat: OrderRepository에 사용자의 주문을 조회하는 메서드 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/OrderRepository.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/entity/OrderRepository.java
@@ -3,6 +3,9 @@ package com.project.yupdduk_clone.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
 }
