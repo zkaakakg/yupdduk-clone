a28fe211ce7a18938d70e9c44a2c99016f9267c3
zkaakakag <cscom222@naver.com>
2025-05-01 17:24:38 +0900

    feat: 관리자용 유저 및 주문 조회 API 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/controller/AdminController.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/controller/AdminController.java
@@ -1,13 +1,18 @@
 package com.project.yupdduk_clone.controller;
 
+import com.project.yupdduk_clone.dto.OrderDto;
 import com.project.yupdduk_clone.dto.StoreDto;
+import com.project.yupdduk_clone.dto.UserDto;
+import com.project.yupdduk_clone.service.OrderService;
 import com.project.yupdduk_clone.service.StoreService;
+import com.project.yupdduk_clone.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,18 +20,20 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController {
     private final StoreService storeService;
+    private final UserService userService;
+    private final OrderService orderService;
 
     @GetMapping
-    public String admin(){
+    public String admin() {
         return "관리자 페이지입니다.";
     }
 
     @PostMapping("/store")
-    public ResponseEntity<?> addStore(@RequestBody StoreDto storeDto){
-        try{
+    public ResponseEntity<?> addStore(@RequestBody StoreDto storeDto) {
+        try {
             storeService.addStore(storeDto);
             return ResponseEntity.status(HttpStatus.CREATED).body(storeDto);
-        }catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(Map.of("error", "이미 등록된 지점입니다."));
@@ -38,4 +45,30 @@ public class AdminController {
 
         }
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> getUserList() {
+        List<UserDto> userDtos = userService.getUserList();
+        return ResponseEntity.ok(userDtos);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDto> getUserDetail(@PathVariable("id") Long userId){
+        UserDto userDto = userService.getUserDetail(userId);
+        return ResponseEntity.ok(userDto);
+
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderDto>> getOrderList(){
+        List<OrderDto> orderDtos = orderService.getOrderList();
+        return ResponseEntity.ok(orderDtos);
+    }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<OrderDto> getOrderDetail(@PathVariable("id") Long orderId){
+        OrderDto orderDto = orderService.getOrderDetail(orderId);
+        return ResponseEntity.ok(orderDto);
+    }
+
 }
