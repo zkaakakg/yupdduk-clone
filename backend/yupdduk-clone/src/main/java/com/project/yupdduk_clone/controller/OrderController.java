870a020d4a8b9d32031c2373613a726e9e375b17
zkaakakag <cscom222@naver.com>
2025-05-01 17:25:40 +0900

    feat: 주문 확인, 수정, 삭제 및 사용자 주문 조회 API 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/controller/OrderController.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/controller/OrderController.java
@@ -2,15 +2,14 @@ package com.project.yupdduk_clone.controller;
 
 import com.project.yupdduk_clone.dto.OrderDto;
 import com.project.yupdduk_clone.dto.OrderItemDto;
+import com.project.yupdduk_clone.dto.OrderRequestDto;
 import com.project.yupdduk_clone.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,9 +19,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<?> createOrder(@RequestBody OrderItemDto orderItemDto){
-        try{
-            OrderDto orderDto = orderService.createOrder(orderItemDto);
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
+        try {
+            OrderDto orderDto = orderService.createOrder(orderRequestDto);
             return ResponseEntity.status(HttpStatus.CREATED).body(orderDto);
 
         } catch (Exception e) {
@@ -30,9 +29,54 @@ public class OrderController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(Map.of("error", "서버 내부 오류가 발생했습니다."));
         }
+    }
 
+    @PutMapping("/{id}/confirm")
+    public ResponseEntity<?> confirmOrder(@PathVariable("id") Long orderId) {
+        try {
+            OrderDto orderDto = orderService.confirmOrder(orderId);
+            return ResponseEntity.ok(orderDto);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", "서버 내부 오류가 발생했습니다."));
+        }
+    }
 
+    @PutMapping("/item/{id}")
+    public ResponseEntity<?> updateOrderItem(@PathVariable("id") Long orderItemId, @RequestBody OrderItemDto orderItemDto) {
+        try {
+            OrderDto orderDto = orderService.updateOrderItem(orderItemId, orderItemDto);
+            return ResponseEntity.ok(orderDto);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", "서버 내부 오류가 발생했습니다."));
+        }
+    }
 
+    @DeleteMapping("/item/{id}")
+    public ResponseEntity<?> deleteOrderItem(@PathVariable("id") Long orderItemId){
+        try{
+            OrderDto orderDto = orderService.deleteOrderItem(orderItemId);
+            return ResponseEntity.ok(orderDto);
+        }catch (Exception e){
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", "서버 내부 오류가 발생했습니다."));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDto>>  getOrderList(){
+        List<OrderDto> orderDtos = orderService.getOrderMyList();
+        return ResponseEntity.ok(orderDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDto> getOrderDetail(@PathVariable("id") Long orderId){
+        OrderDto orderDto = orderService.getOrderDetail(orderId);
+        return ResponseEntity.ok(orderDto);
     }
 
 }
