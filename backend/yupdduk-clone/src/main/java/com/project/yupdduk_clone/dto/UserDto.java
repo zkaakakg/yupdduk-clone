7742c78e10ef0558eccaa5c254db95d91ab2e060
zkaakakag <cscom222@naver.com>
2025-05-01 17:27:52 +0900

    feat: UserDto에 생성자 추가

--- backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/dto/UserDto.java
+++ backend/yupdduk-clone/src/main/java/com/project/yupdduk_clone/dto/UserDto.java
@@ -31,6 +31,15 @@ public class UserDto {
         this.userRole = user.getUserRole();
     }
 
+    public UserDto(Long id, String name, String email, UserRole userRole) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.userRole = userRole;
+    }
+
     public User toEntity() {
         return User.builder()
                 .name(name)
