cf9f26818f739c1b1c0bacb83824add680249320
gaeun.jang <cscom222@naver.com>
2025-05-03 01:52:01 +0900

    refactor: 뒤로가기 버튼에 useNavigate 적용

--- frontend/yupdduk-clone/src/components/Header.jsx
+++ frontend/yupdduk-clone/src/components/Header.jsx
@@ -1,14 +1,21 @@
 import 뒤로가기 from "../assets/뒤로가기.png";
 import styles from "../styles/Header.module.css";
-
+import { useNavigate } from "react-router-dom";
 const Header = (props) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <header className={styles.header}>
-      <button className={styles.backButton} onClick={props.ClickFunc}>
-        <img src={뒤로가기} alt="" className={styles.backButtonImg} />
+      <button className={styles.backButton} onClick={handleBack}>
+        <img src={뒤로가기} alt="뒤로가기" className={styles.backButtonImg} />
       </button>
       <p className={styles.title}>{props.title}</p>
     </header>
   );
 };
+
 export default Header;
