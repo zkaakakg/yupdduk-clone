e3153fb38012154dfc9fb2e5bf08ebb6a1b882b8
gaeun.jang <cscom222@naver.com>
2025-05-03 01:52:38 +0900

    feat: 마이페이지, 주문 목록 및 주문 상세 페이지 라우팅 추가

--- frontend/yupdduk-clone/src/main.jsx
+++ frontend/yupdduk-clone/src/main.jsx
@@ -7,6 +7,9 @@ import MainPage from "./pages/MainPage";
 import JoinPage1 from "./pages/JoinPage1";
 import JoinPage2 from "./pages/JoinPage2";
 import JoinPage3 from "./pages/JoinPage3";
+import MyPage from "./pages/MyPage";
+import MyOrderListPage from "./pages/MyOrderListPage";
+import MyOrderDetailPage from "./pages/MyOrderDetailPage";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -16,6 +19,9 @@ createRoot(document.getElementById("root")).render(
       <Route path="/join1" element={<JoinPage1 />} />
       <Route path="/join2" element={<JoinPage2 />} />
       <Route path="/join3" element={<JoinPage3 />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/my-order" element={<MyOrderListPage />} />
+      <Route path="/my-order/:orderId" element={<MyOrderDetailPage />} />
       <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
     </Routes>
   </BrowserRouter>
