d48b597252415b62ea59ddd8aa92f07388fa8f84
gaeun.jang <cscom222@naver.com>
2025-05-04 22:32:49 +0900

    feat: 관리자 페이지 탭 메뉴 및 로그아웃 기능 추가

---
+++ frontend/yupdduk-clone/src/pages/AdminPage.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import styles from "../styles/AdminPage.module.css";
+import StoreTab from "../components/StoreTab";
+import OrderListTab from "../components/OrderListTab";
+import UserListTab from "../components/UserListTab";
+import { useNavigate } from "react-router-dom";
+
+const AdminPage = () => {
+  const navigate = useNavigate();
+  const [activeMenu, setActiveMenu] = useState("store");
+
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+      if (response.ok) {
+        alert("로그아웃이 완료되었습니다.");
+        navigate("/");
+      } else {
+        console.error("서버 에러", response);
+        alert("로그이웃에 실패했습니다.");
+      }
+    } catch (err) {
+      console.error("네트워크 에러", err);
+      alert("요청 중 오류가 발생했습니다.");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <div
+            className={`${styles.menuItem} ${
+              activeMenu === "store" ? styles.active : ""
+            }`}
+            onClick={() => handleMenuClick("store")}
+          >
+            매장 관리
+          </div>
+          <div
+            className={`${styles.menuItem} ${
+              activeMenu === "members" ? styles.active : ""
+            }`}
+            onClick={() => handleMenuClick("members")}
+          >
+            회원 목록
+          </div>
+          <div
+            className={`${styles.menuItem} ${
+              activeMenu === "orders" ? styles.active : ""
+            }`}
+            onClick={() => handleMenuClick("orders")}
+          >
+            주문 목록
+          </div>
+        </nav>
+        <div className={styles.logout}>
+          <img
+            src="https://assets.streamlinehq.com/image/private/w_300,h_300,ar_1/f_auto/v1/icons/c4/logout-31dva2zl3l9ao92pwnmgnt.png/logout-6bjs3s3fnciv7c0ye52y3l.png?_a=DATAdtAAZAA0"
+            alt=""
+            style={{ width: "20px" }}
+          />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </header>
+
+      <main className={styles.mainContent}>
+        {activeMenu === "store" && <StoreTab />}
+        {activeMenu === "members" && <UserListTab />}
+        {activeMenu === "orders" && <OrderListTab />}
+      </main>
+    </div>
+  );
+};
+
+export default AdminPage;
